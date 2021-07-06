7f2851ba5d9632aabaf10f1fd42517b0f721b1eb
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-07-06 13:03:56 -0500

    Add pay calculator program

--- js/external.js
+++ js/external.js
@@ -1,3 +1,43 @@
 "use strict";
 
 console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+var faveColor = prompt("What is your favorite color?");
+
+alert("Great, " + faveColor + " is my favorite color too!");
+
+
+// Movie Rental Program
+
+// var numberOfMovies = prompt("How many movies have you rented?");
+// var daysRented = prompt("How many days are you renting them for?");
+//
+// function totalCostInDollars() {
+//     alert("Your total cost is $" + numberOfMovies * daysRented);
+// }
+//
+// totalCostInDollars();
+
+
+// Pay Calculator Program
+
+var googlePay = prompt("How much do you get paid per hour at Google?");
+var googleHours = prompt("How many hours did you work this week at Google?");
+
+var amazonPay = prompt("How much do you get paid per hour at Amazon?");
+var amazonHours = prompt("How many hours did you work this week at Amazon");
+
+var facebookPay = prompt("How much do you get paid per hour at Facebook?");
+var facebookHours = prompt("How many hours did you work this week at Facebook?");
+
+function totalPayThisWeek() {
+    var googleWeeklyEarnings = googlePay * googleHours;
+    var amazonWeeklyEarnings = amazonPay * amazonHours;
+    var facebookWeeklyEarnings = facebookPay * facebookHours;
+
+    alert("You made a total of $" + (googleWeeklyEarnings + amazonWeeklyEarnings + facebookWeeklyEarnings) + " this week!");
+}
+
+totalPayThisWeek();

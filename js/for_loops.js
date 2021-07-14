3bc08c8f320f504d054378b6d61ab866a1aed3da
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-07-14 12:30:32 -0500

    Add while loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// function showMultiplicationTable(number) {
+//     console.log(number + " x 1 = " + (number * 1));
+// }
+//
+// showMultiplicationTable();
---
+++ js/while.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// var num = 2;
+// while (num <= 65536) {
+//     console.log(num);
+//     num = num * 2;
+// }
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+console.log("We have " + allCones + " cones in stock.");
+
+do {
+    var conesBought = Math.floor(Math.random() * 5) + 1;
+
+    if (conesBought <= allCones) {
+        allCones = allCones - conesBought;
+        console.log(conesBought + " cones sold...");
+
+    } else {
+        console.log("Sorry! I can't sell you " + conesBought + " cones.");
+    }
+
+
+
+    console.log("We have " + allCones + " cones left.");
+} while (conesBought < allCones);
+
+console.log("Yay! I sold them all!");

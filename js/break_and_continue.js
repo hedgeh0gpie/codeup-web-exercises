55125d7aba2f2e39ae9797840db31c3fc7c23cb0
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-08-02 09:50:05 -0500

    Rename positioning_exercise.html to position-exercise.html

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -6,3 +6,15 @@ while ((oddNumber % 2 === 0) || (oddNumber > 50) || (oddNumber < 1)) {
     alert("Stop playing around.");
     var oddNumber = prompt("Please enter an odd number between 1 and 50.");
 }
+
+for (var i = 1; i <= 50; i++) {
+
+    if (i % 2 === 0) {
+        continue;
+
+    }
+    if (i === oddNumber) {
+        console.log("Skipping number " + oddNumber);
+    }
+    console.log(i);
+}
